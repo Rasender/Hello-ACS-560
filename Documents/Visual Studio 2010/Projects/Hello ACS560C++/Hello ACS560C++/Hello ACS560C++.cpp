c28fef8b6767afc3ab43d5b137303913636a1fe5
Rasender <rasender2636@yahoo.com>
2014-09-15 08:35:37 -0400

    hello in c++

---
+++ Documents/Visual Studio 2010/Projects/Hello ACS560C++/Hello ACS560C++/Hello ACS560C++.cpp
@@ -0,0 +1,14 @@
+// Hello ACS560C++.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Hello ACS560!";
+	cin.get();
+	return 0;
+}
+
